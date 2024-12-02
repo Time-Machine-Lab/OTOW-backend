4c964edb834032818b755a43bd55672f2795bb39
li <3250162035@qq.com>
2024-12-02 12:56:17 +0800

    init

---
+++ src/main/java/com/tml/otowbackend/OtowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tml.otowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtowBackendApplication.class, args);
+    }
+
+}
