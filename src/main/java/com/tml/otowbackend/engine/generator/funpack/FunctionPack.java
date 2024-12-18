fab58816248095baf88f75b30782499fb4f3aa2f
曾强 <2980419682@qq.com>
2024-12-18 11:20:14 +0800

    重构增删改查模板的动态生成

--- src/main/java/com/tml/otowbackend/engine/generator/funpack/FunctionPack.java
+++ src/main/java/com/tml/otowbackend/engine/generator/funpack/FunctionPack.java
@@ -4,13 +4,9 @@ package com.tml.otowbackend.engine.generator.funpack;
 public interface FunctionPack {
 
     Object generateController(Object object);
-
     Object generateService(Object object);
-
     Object generateServiceImpl(Object object);
-
     Object generateMapper(Object object);
-
     Object generateApplication(Object object);
     Object generateApplicationConfig(Object object);
     Object generatePomConfig(Object object);
